85cca98074feb3bcf00e1f5fd7417dec85afaba1
ma-pedroza <matheusgomeszpedroza@gmail.com>
2024-11-14 19:16:08 -0300

    commit 01 14.11
    fiz uma tela visual simples de cadastro de categoria, não está 100% funcional

---
+++ add_cat.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar - Categoria de Gasto</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-sm-7">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="text-center">
+                            Adicionar nova categoria de gasto
+                            <a href="index.php" class="btn btn-danger float-start"><i class="bi bi-arrow-return-left"></i></a>
+                        </h4>
+                        
+                    </div>
+                    <div class="card-body">
+                        <form action="acoes.php" method="POST">
+                            <div class="mb-3">
+                                <label for="txtNome">Nome</label>
+                                <input type="text" name="txtNome" id="txtNome" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <button name="create-categoria" type="submit" class="btn btn-success float-end"><i class="bi bi-floppy2-fill">  Salvar</i></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+    
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
