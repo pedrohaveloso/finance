144334fa1ed9ac50f1a60ec2935fa2a3242aaf0c
Roddy192 <149844471+Roddy192@users.noreply.github.com>
2024-11-22 22:13:26 -0300

    Rodrigo - 22-11 - Resumo mes version 1

--- acoes.php
+++ acoes.php
@@ -10,16 +10,6 @@ if (isset($_POST['create_transacao'])){
     $id_mes= trim($_POST['idMes']);
     $categoria = trim($_POST['txtCat']);
 
-    if($tipo == 'output')
-    {
-        $valor = $valor * (-1);
-
-    }
-    elseif($tipo == 'input')
-    {
-        $valor = $valor * (1);
-
-    }
 
     $sql="INSERT INTO transaction (date, type, description, value, month_id) VALUES ('$data', '$tipo', '$descricao', '$valor', '$id_mes')";
     $sqlinsert = mysqli_query($conn,$sql);
